c56d72be806513c91bc57ac4eb5bdb53482aa996
blademax88 <159867596+blademax88@users.noreply.github.com>
2024-03-19 09:33:26 +0530

    Add files via upload

---
+++ dev_arch/src/main/java/dev_group/dev_arch/App.java
@@ -0,0 +1,13 @@
+package dev_group.dev_arch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
